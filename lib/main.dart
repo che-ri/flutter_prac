9dba5193f12eb0931990ee329a317ccabe6849d5
che-ri <rnjs1135@gmail.com>
2022-09-01 09:54:37 +0900

    feat ::: 플러터 생명주기

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,6 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    print('createState');
     return _MyApp();
   }
 }
@@ -13,8 +16,19 @@ class _MyApp extends State<MyApp> {
   var switchValue = false;
   String test = "hello";
   Color _color = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
+    print('build');
+
+    if (mounted == true) {
+      print('mounted==true');
+    }
+
+    if (mounted == false) {
+      print('mounted==false');
+    }
+
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -30,11 +44,13 @@ class _MyApp extends State<MyApp> {
             onPressed: () {
               if (_color == Colors.blue) {
                 setState(() {
+                  print('setState');
                   test = 'flutter';
                   _color = Colors.amber;
                 });
               } else {
                 setState(() {
+                  print('setState');
                   test = 'hello';
                   _color = Colors.blue;
                 });
@@ -43,4 +59,55 @@ class _MyApp extends State<MyApp> {
           )),
         ));
   }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print('didChangeDependencies');
+  }
+
+  @override
+  void didUpdateWidget(covariant MyApp oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print('didUpdateWidget');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactivate');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('dispose');
+  }
 }
+
+/**
+ * 첫 렌더링시 콘솔
+ * I/flutter (11651): createState
+ * I/flutter (11651): initState
+ * I/flutter (11651): didChangeDependencies
+ * I/flutter (11651): build
+ * I/flutter (11651): mounted==true
+ */
+
+/**
+ * 업데이트시 콘솔
+ * I/flutter (11651): setState
+ * I/flutter (11651): build
+ * I/flutter (11651): mounted==true
+ */
