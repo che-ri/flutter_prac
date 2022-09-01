15d32c172848dab7cca5e01704cdef459fcbec99
che-ri <rnjs1135@gmail.com>
2022-09-01 23:50:39 +0900

    feat ::: 리스트 추가페이지 : 동물 이미지 선택 => 가로 스크롤로 변경

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/rendering/box.dart';
 import './animalItem.dart';
 
 class SecondApp extends StatefulWidget {
@@ -27,7 +28,7 @@ class _SecondAppState extends State<SecondApp> {
         child: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: <Widget>[
             TextField(
                 controller: nameController,
                 keyboardType: TextInputType.text,
@@ -36,10 +37,10 @@ class _SecondAppState extends State<SecondApp> {
               children: [
                 Radio(
                     value: 0, groupValue: _radioValue, onChanged: _radioChange),
-                Text('양서류'),
+                Text('곤충'),
                 Radio(
                     value: 1, groupValue: _radioValue, onChanged: _radioChange),
-                Text('파충류'),
+                Text('영장류'),
                 Radio(
                     value: 2, groupValue: _radioValue, onChanged: _radioChange),
                 Text('포유류'),
@@ -59,34 +60,61 @@ class _SecondAppState extends State<SecondApp> {
               ],
               mainAxisAlignment: MainAxisAlignment.spaceAround,
             ),
-            Row(
-              children: [
-                GestureDetector(
-                  child: Image.asset('image/cow.png', width: 80),
-                  onTap: () {
-                    _imagePath = 'image/cow.png';
-                  },
-                ),
-                GestureDetector(
-                  child: Image.asset('image/pig.png', width: 80),
-                  onTap: () {
-                    _imagePath = 'image/pig.png';
-                  },
-                ),
-                GestureDetector(
-                  child: Image.asset('image/bee.png', width: 80),
-                  onTap: () {
-                    _imagePath = 'image/bee.png';
-                  },
-                ),
-                GestureDetector(
-                  child: Image.asset('image/cat.png', width: 80),
-                  onTap: () {
-                    _imagePath = 'image/cat.png';
-                  },
-                ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            Container(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  GestureDetector(
+                    child: Image.asset('image/bee.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/bee.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/cat.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/cat.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/cow.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/cow.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/dog.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/dog.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/fox.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/fox.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/monkey.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/monkey.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/pig.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/pig.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('image/wolf.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'image/wolf.png';
+                    },
+                  ),
+                ],
+              ),
             ),
             ElevatedButton(onPressed: () {}, child: Text('동물 추가하기'))
           ],
