42c57d0ff14daaf6904b50ab08d8aba02b5f40c2
che-ri <rnjs1135@gmail.com>
2022-09-01 22:35:59 +0900

    feat ::: 네비게이션

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-// import 'dart:html';
-
-import 'package:app/imageWidget.dart';
+import 'package:app/firstPage.dart'; //첫번째 페이지
+import 'package:app/secondPage.dart'; //두번째 페이지
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,103 +17,60 @@ class WidgetApp extends StatefulWidget {
   _WidgetApp createState() => _WidgetApp();
 }
 
-class _WidgetApp extends State<WidgetApp> {
-  String sum = "";
-  TextEditingController value1 = TextEditingController();
-  TextEditingController value2 = TextEditingController();
-  String? _buttonText;
-  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
-  List<DropdownMenuItem<String>> _dropDownMenuItems =
-      new List.empty(growable: true);
+class _WidgetApp extends State<WidgetApp> with SingleTickerProviderStateMixin {
+  //with 키워드는 여러 클래스를 재사용하게끔 해준다.
+  //SingleTickerProviderStateMixin을 추가로 상속하면서 위젯이 **애니메이션 동작**을 처리할 수 있게 한 것임. 상속하지 않으면 탭컨트롤러를 만들 수 없음.
+
+  TabController? controller; //여러 화면을 이동하는 역할을 하므로 기본적으로 **애니메이션**을 사용한다.
   @override
   void initState() {
     super.initState();
-    for (var item in _buttonList) {
-      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
-    }
-    _buttonText = _dropDownMenuItems[0].value;
+    controller = TabController(
+        length: 2,
+        vsync:
+            this); //length: 탭의 갯수, vsync : 탭이 이동했을 때 호출되는 콜백 함수를 어디서 처리할지 지정.
+  }
+
+  @override
+  void dispose() {
+    controller!
+        .dispose(); //탭 컨트롤러는 애니메이션을 사용하므로, dispose() 함수를 호출해줘야 메모리 누수를 막을 수 있다.
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    controller!.addListener(() => {
+          //탭이 이동할 때 어떠한 액션을 취하고 싶으면 addListener을 사용한다.
+          if (!controller!.indexIsChanging)
+            //indexIsChanging은 탭이 현재 이동 중인지를 나타낸다.
+            {
+              print('prev ::: ${controller!.previousIndex}'),
+              print('cur ::: ${controller!.index}')
+            }
+        });
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-          child: Center(
-              child: Column(
-        children: <Widget>[
-          Padding(
-              padding: EdgeInsets.all(15),
-              child: Text('Flutter',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 30,
-                      fontFamily: 'Pacifico'))),
-          Padding(
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: value1,
-              )),
-          Padding(
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: value2,
-              )),
-          Padding(
-              padding: EdgeInsets.all(15),
-              child: DropdownButton(
-                  items: _dropDownMenuItems,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _buttonText = value;
-                    });
-                  },
-                  value: _buttonText)),
-          Padding(
-              padding: EdgeInsets.all(15),
-              child: ElevatedButton(
-                  onPressed: () => {
-                        setState(() {
-                          var value1Int = num.parse(value1.value.text);
-                          var value2Int = num.parse(value2.value.text);
-                          var result;
-                          switch (_buttonText) {
-                            case "더하기":
-                              result = value1Int + value2Int;
-                              break;
-                            case "빼기":
-                              result = value1Int - value2Int;
-                              break;
-                            case "곱하기":
-                              result = value1Int * value2Int;
-                              break;
-                            case "나누기":
-                              result = value1Int / value2Int;
-                              break;
-                            default:
-                              result = 0;
-                              break;
-                          }
-                          sum = '$result';
-                        })
-                      },
-                  child: Row(
-                    children: [Icon(Icons.add), Text(_buttonText!)],
-                  ),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.amber)))),
-          Padding(
-              padding: EdgeInsets.all(15),
-              child: Text(
-                '결과 : $sum',
-                style: TextStyle(fontSize: 20, color: Colors.grey),
-              )),
-        ],
-      ))),
-    );
+        appBar: AppBar(
+          title: Text('Flutter Prac'),
+        ),
+        body: TabBarView(
+          children: <Widget>[FirstApp(), SecondApp()],
+          controller: controller,
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: <Tab>[
+            Tab(
+                icon: Icon(
+              Icons.looks_one,
+              color: Colors.blue,
+            )),
+            Tab(
+                icon: Icon(
+              Icons.looks_two,
+              color: Colors.blue,
+            ))
+          ],
+          controller: controller,
+        ));
   }
 }
