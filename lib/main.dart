9fcd59132e33d089830c8dbda7f4f8cb102d799c
che-ri <rnjs1135@gmail.com>
2022-09-07 12:19:06 +0900

    feat ::: 앱데이터를 공유 환경설정 파일에 저장하기

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:app/secondDetail.dart';
 import 'package:app/subDetail.dart';
 import 'package:app/thirdDetail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   await dotenv.load(fileName: 'config/.env');
@@ -16,26 +17,87 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: CountApp());
+  }
+}
+
+class CountApp extends StatefulWidget {
+  const CountApp({super.key});
+
+  @override
+  State<CountApp> createState() => _CountAppState();
+}
+
+class _CountAppState extends State<CountApp> {
+  int count = 0;
+
+  void _setData(int value) async {
+    //공유 환경설정에 저장
+    var key = 'count';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setInt(key, value);
+  }
+
+  void _loadData() async {
+    //공유 환경설정에서 불러오기
+    var key = 'count';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      var value = pref.getInt(key);
+      if (value == null) {
+        count = 0;
+      } else {
+        count = value;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('카운터 앱')),
+      body: Container(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('count'),
+            Text(
+              '$count',
+              style: TextStyle(fontSize: 30),
+            )
+          ],
+        )),
       ),
-      //route설정
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SubDetail(),
-        '/second': (context) => SecondDetail(),
-        '/third': (context) => ThirdDetail()
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+            _setData(count);
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
